cd5edec2c2795f1aa735065c9f690f7755f30c7e
Mike Landers <landers.mike@gmail.com>
2019-05-31 08:33:02 -0700

    added define's

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 #include <dirent.h>
 
+#define MAX_COMMANDLINE_SIZE 2048
+#define MAX_TOKENS 100
+#define PROMPT "shell$ "
+
+// Parse Command Line Function
+char **parse_commandline(char *commandline, char **args, int *args_count)
+{
+  char *token;
+
+  *args_count = 0;
+  token = strtok(commandline, " \t\n\r");
+
+  while (token != NULL)
+  {
+    args[*args_count] = token;
+    (*args_count)++;
+    token = strtok(NULL, "/");
+  }
+
+  args[*args_count] = NULL;
+  return args;
+}
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char commandline[MAX_COMMANDLINE_SIZE];
+  char *args[MAX_TOKENS];
+  int args_count;
+  printf("%s", PROMPT);
+  fgets(commandline, sizeof(commandline), stdin);
   // Parse command line
-  char **parse_commandline(char *commandline, char **args, int *args_count)
-  {
-    char *token;
-
-    *args_count = 0;
-    token = strtok(commandline, " \t\n\r");
-
-    while (token != NULL)
-    {
-      args[*args_count] = token;
-      (*args_count)++;
-      token = strtok(NULL, "/");
-    }
-
-    args[*args_count] = NULL;
-    return args;
-  }
-
+  parse_commandline(commandline, args, &args_count);
   // Open directory
 
   // Repeatly read and print entries
